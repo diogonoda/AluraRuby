9ecfcef8b26b674293929e3816e5ac003ce69f30
Diogo Noda <diogonoda@hotmail.com>
2016-05-09 00:29:56 -0300

    Durante a segunda aula

--- Ruby/maior_ou_menor.rb
+++ Ruby/maior_ou_menor.rb
@@ -1,26 +1,24 @@
-puts "Bim vindo ao jogo da adivinhação"
+puts "Bem vindo ao jogo da adivinhação"
 puts "Qual é o seu nome?"
 nome = gets
 
-puts
-puts
-puts
-puts
-puts
-puts
-puts
+puts "\n\n\n\n\n\n"
 puts "Começaremos o jogo para você, " + nome
 
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... Adivinhe o número secreto!"
 
-puts
-puts
-puts
-puts
+puts "\n\n\n\n\n"
 puts "Tentativa 1"
 puts "Entre com um número"
 chute = gets
 puts "Será que acertou? Você chutou " + chute
-puts chute.to_i == numero_secreto
+
+acertou = chute.to_i == numero_secreto
+
+if acertou
+  puts "Acertou!"
+else
+  puts "Errou!"
+end
