cb200f7fb2cdbcd135e6d93353ed8e5d4494432c
Diogo Noda <diogonoda@hotmail.com>
2016-05-09 23:55:38 -0300

    Finalizados os videos da aula 2

--- Ruby/maior_ou_menor.rb
+++ Ruby/maior_ou_menor.rb
@@ -10,22 +10,27 @@ puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... Adivinhe o número secreto!"
 
-puts "\n\n\n\n\n"
-puts "Tentativa 1"
-puts "Entre com um número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
+limite_de_tentativas = 5
 
-acertou = chute.to_i == numero_secreto
+for tentativa in 1..limite_de_tentativas
+  puts "\n\n\n\n\n"
+  puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+  puts "Entre com um número"
+  chute = gets
+  puts "Será que acertou? Você chutou " + chute
 
-if acertou
-  puts "Acertou!"
-else
-  maior = numero_secreto > chute.to_i
+  acertou = chute.to_i == numero_secreto
 
-  if maior
-    puts "O número secreto é maior!"
+  if acertou
+    puts "Acertou!"
+    break
   else
-    puts "O número secreto é menor!"
+    maior = numero_secreto > chute.to_i
+
+    if maior
+      puts "O número secreto é maior!"
+    else
+      puts "O número secreto é menor!"
+    end
   end
 end
