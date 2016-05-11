6a2192da7e4f8818f08dd393492082a00dd6706b
= <=>
2016-05-11 15:33:34 -0300

    Refatorando e testando funcoes

--- Ruby/maior_ou_menor.rb
+++ Ruby/maior_ou_menor.rb
@@ -1,36 +1,62 @@
 # encoding: ISO-8859-1
-puts "Bem vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
+def da_boas_vindas
+  puts "Bem vindo ao jogo da adivinhação"
+  puts "Qual é o seu nome?"
+  nome = gets
 
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
+  puts "\n\n\n\n\n\n"
+  puts "Começaremos o jogo para você, " + nome
+end
 
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... Adivinhe o número secreto!"
+def sorteia_numero_secreto
+  puts "Escolhendo um número secreto entre 0 e 200..."
+  sorteado = 175
+  puts "Escolhido... Adivinhe o número secreto!"
 
-limite_de_tentativas = 5
+  sorteado
+end
 
-for tentativa in 1..limite_de_tentativas
+def pede_um_numero(tentativa, limite_de_tentativas)
   puts "\n\n\n\n\n"
   puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
   puts "Entre com um número"
   chute = gets
   puts "Será que acertou? Você chutou " + chute
 
-  acertou = chute.to_i == numero_secreto
+  chute.to_i
+end
+
+def verifica_se_acertou(chute, numero_secreto)
+  acertou = chute == numero_secreto
 
   if acertou
     puts "Acertou!"
-    break
-  else
-    maior = numero_secreto > chute.to_i
 
-    if maior
-      puts "O número secreto é maior!"
-    else
-      puts "O número secreto é menor!"
-    end
+    return true
+  end
+
+  maior = numero_secreto > chute
+
+  if maior
+    puts "O número secreto é maior!"
+  else
+    puts "O número secreto é menor!"
+  end
+
+  false
+end
+
+
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+  chute = pede_um_numero(tentativa, limite_de_tentativas)
+
+  if verifica_se_acertou(chute, numero_secreto)
+    break
   end
 end
