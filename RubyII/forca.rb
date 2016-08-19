4eba2b52dcae9c8efbfd92250d9bc9ef9c98eda4
Diogo Noda <diogonoda@hotmail.com>
2016-08-19 00:46:53 -0300

    Finalizando a primeira aula

--- RubyII/forca.rb
+++ RubyII/forca.rb
@@ -43,6 +43,13 @@ def joga(nome)
 
   while erros < 5
     chute = pede_um_chute chutes, erros
+
+    if chutes.include? chute
+      puts "Você já chutou #{chute}"
+
+      next
+    end
+
     chutes << chute
 
     chutou_uma_letra = chute.size == 1
