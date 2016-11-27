5c213a1a83dd2cb165be4867334d6ff5f04193db
Diogo Noda <diogonoda@hotmail.com>
2016-11-27 01:20:27 -0200

    Ponto 1

---
+++ FogeFoge/fogefoge.rb
@@ -0,0 +1,21 @@
+require_relative 'ui'
+
+def le_mapa(numero)
+  arquivo = "mapa#{numero}.txt"
+  texto = File.read arquivo
+  mapa = texto.split "\n"
+end
+
+def joga(nome)
+  mapa = le_mapa 1
+
+  while true
+    desenha mapa
+    direcao = pede_movimento
+  end
+end
+
+def inicia_fogefoge
+  nome = da_boas_vindas
+  joga nome
+end
